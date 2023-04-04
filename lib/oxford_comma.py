6cfbbee0475bf54ec62ee6bfa0939fdbd0e24952
Erkut Cakmak <cakmak.erkut1@gmail.com>
2023-04-04 15:07:32 +0300

    lab complete

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,9 @@
 def oxford_comma(items):
-    return None
+    if len(items) == 1:
+        return items[0]
+    elif len(items) == 2:
+        return "{} and {}".format(items[0], items[1])
+    elif len(items) == 3:
+        return "{}, {}, and {}".format(items[0], items[1], items[2])
+    else:
+        return f"{', '.join(items[:-1])}, and {items[-1]}"
